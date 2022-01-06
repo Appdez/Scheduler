b35d1b7d1a46a3e2e4abe7accd02e0ada5803d5c
Nelson Mutane <57417802+inkomomutane@users.noreply.github.com>
2022-01-06 10:40:22 +0200

    Models cleaned

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,11 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User
@@ -26,8 +31,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  */
-class User extends Model
+class User extends Authenticatable
 {
+	use HasApiTokens, HasFactory, Notifiable;
 	protected $table = 'users';
 
 	protected $dates = [
